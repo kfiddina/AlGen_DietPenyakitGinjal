3f9f6a5c03d6fcb0a134b67c8d6f9de605e34930
khalishahf <57613669+kfook@users.noreply.github.com>
2021-12-10 00:42:38 +0700

    Delete pages Directory
    Too complicated. Simplified version: index.php

--- pages/404.php
+++
@@ -1,2 +0,0 @@
-	<h3>404 Not Found!</h3>
-	<p>Maaf, halaman yang anda cari tidak tersedia</p>
--- pages/home.php
+++
@@ -1,8 +0,0 @@
-<div class="text-center mt-5">
-	<h1>Menentukan Bahan Makanan <br/>Bagi Penderita Gagal Ginjal Akut</h1>
-	<p class="lead">Project Algoritma Genetika</p>
-	<p>Kelompok 4</p>
-	<p>
-		<a href="?p=periksa_view">Periksa</a>
-	</p>
-</div>
